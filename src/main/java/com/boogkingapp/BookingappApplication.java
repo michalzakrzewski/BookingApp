2baef2ab79be7084ba6a414185c112398351e8a5
Michał Zakrzewski <michalzakrzewski@users.noreply.github.com>
2018-06-29 13:20:57 +0200

    new project

---
+++ src/main/java/com/boogkingapp/BookingappApplication.java
@@ -0,0 +1,12 @@
+package com.boogkingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingappApplication.class, args);
+    }
+}
---
+++ src/test/java/com/boogkingapp/BookingappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.boogkingapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookingappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
