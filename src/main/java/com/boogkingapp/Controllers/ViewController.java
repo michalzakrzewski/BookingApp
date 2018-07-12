ec0bda4e7b108e719d9287e75321603b01f20ae5
Michał Zakrzewski <michalzakrzewski@users.noreply.github.com>
2018-07-12 21:31:18 +0200

    add template html

--- src/main/java/com/boogkingapp/Controllers/ViewController.java
+++ src/main/java/com/boogkingapp/Controllers/ViewController.java
@@ -1,12 +1,17 @@
 package com.boogkingapp.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 @Controller
 public class ViewController {
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "Michał Zakrzewski");
         return "index";
     }
 }
