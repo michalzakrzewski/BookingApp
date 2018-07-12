ca27ae0323afba72ed9ea4c2334bc0d5e6dd80fb
Michał Zakrzewski <michalzakrzewski@users.noreply.github.com>
2018-07-12 22:07:12 +0200

    add AngularJS

---
+++ src/main/resources/static/app/bookings.controller.js
@@ -0,0 +1,11 @@
+(function () {
+   'use strict';
+
+   angular.module('app').controller('BookingsController', BookingsController);
+
+   BookingsController.$inject = [];
+
+   function BookingController() {
+       var vm = this;
+   }
+});
